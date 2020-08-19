50ab3a06469b2e71d6d37a07b67fd72aa7e76dd7
stevekutz <stkutz@gmail.com>
2020-08-19 13:53:51 -0500

    added dict copy by value examples

--- dict_copying.py
+++ dict_copying.py
@@ -1,4 +1,4 @@
-new_dict = {'first': 1, 'second': 2, 'third': 3, 'fourth': 4}
+orig_dict = {'first': 1, 'second': 2, 'third': 3, 'fourth': 4}
 
 # function to get keys
 # def get_keys(**kw):
@@ -6,18 +6,39 @@ new_dict = {'first': 1, 'second': 2, 'third': 3, 'fourth': 4}
 
 # get_keys( a=1, b=2)  # kw.keys dict_keys(['a', 'b'])
 
-dict_2 = new_dict.copy()  # makes SHALLOW copy
-print(dict_2)
-dict_2['first'] = 100
-print(new_dict)
-print(dict_2)
 
-dict_c = dict( (k,v) for k,v in dict_2.items() )
+#### SHALLOW COPY method to copy by VALUE
+dict_sc = orig_dict.copy()  # makes SHALLOW copy
+dict_sc['first'] = 100  # changed value at key 'first'
+print(f'  orig_dict >> {orig_dict}')   #  orig_dict >> {'first': 1, 'second': 2, 'third': 3, 'fourth': 4}
+# print(f'  dict_2 >> {dict_sc} ')       #  dict_2 >> {'first': 100, 'second': 2, 'third': 3, 'fourth': 4}
 
+#### DICT COMPREHENSION method to copy by VALUE
+# copy dict using dict comprehension
+dict_c = dict( (k,v) for k,v in orig_dict.items() )
+print(f' orig orig_dict mem  {id(orig_dict)}  AND copied dict_c mem {id(dict_c)}')  
+# ex . . note different mem addresses     
+# orig orig_dict mem  140401682401920  AND copied dict_c mem 140401693896064
+dict_c['second'] = "TWO" 
+print(f' NOW dict_c is {dict_c}')   #  NOW dict_c is {'first': 1, 'second': 'TWO', 'third': 3, 'fourth': 4}
 
+# Function to iterate through k,v tuple pairs
 def get_kv(dict_kw):
     # kv_pairs = dict(kv.items())
+    for kv_pair in dict_kw.items():
+        print(f' key >> {kv_pair[0]} \t value >> {kv_pair[1]}')
+
+    print('')
     for k,v in dict_kw.items():
-        print(f' key {k} value {v}')
+        print(f' key >> {k}\t:  \tvalue >> {v}')
 
 get_kv(dict_c)
+# key >> first    value >> 1
+# key >> second   value >> 2
+# key >> third    value >> 3
+# key >> fourth   value >> 4
+
+# key >> first   :       value >> 1
+# key >> second  :       value >> 2
+# key >> third   :       value >> 3
+# key >> fourth  :       value >> 4
