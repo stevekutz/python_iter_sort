5b6474507bff5a1b64c071e87c29a1fa3a625f89
stevekutz <stkutz@gmail.com>
2020-09-11 20:40:18 -0500

    added func to determine which bit is set in a given byte

--- conversion.py
+++ conversion.py
@@ -32,10 +32,48 @@ print(f' hex(10)  {hex(10)}')  # 0xa
 
 bin_a = 0b10001001
 shift_right = bin_a >> 1
-print(f' orig {bin(bin_a)}  shift_right {bin(shift_right)}')
-
+print(f' orig {bin(bin_a)}  shift_right {bin(shift_right)} \n')
+print(f' orig {bin(bin_a)}')
 
 print(f' str(bin_a)[0]  {str(bin_a)[0]} {type(str(bin_a)[0])}')
 print(f' int(str(bin_a)[0])  {int(str(bin_a)[0])} {type(int(str(bin_a)[0]))}')
 
 print(f' bin(bin_a)  {bin(bin_a)} {type(str(bin_a))}')
+
+
+bit_p = 0b10000111
+bit_p2 =0b01010100
+
+def bit_set(bin_val, position = None):
+    pos = [1, 2, 4, 8, 16, 32, 64, 128]
+
+    if position != None:
+        if bin_val & pos[position] != 0:
+            return 1
+        else:
+            return 0    
+
+
+    else:
+        for i in range(8):
+            if bin_val & pos[i] != 0:
+                print(f' bit {i} is set to 1 ')  
+            else:
+                print(f' bit {i} is set to 0')          
+
+
+# for i in range(8):
+#     print(f' bit at pos {i} is {bit_set(bit_p, i)}')   # bit_p = 0b10000111
+# bit at pos 0 is 1
+# bit at pos 1 is 1
+# bit at pos 2 is 1
+# bit at pos 3 is 0
+# bit at pos 4 is 0
+# bit at pos 5 is 0
+# bit at pos 6 is 0
+# bit at pos 7 is 1
+
+for i in range(8):
+    print(f' bit at pos {i} is {bit_set(bit_p2, i)}')   # bit_p2 =0b01010100
+
+print(f' 0b010 {0b0000010}')
