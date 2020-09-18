1da794981d5c2464633b9f349216bbbd418b7e54
stevekutz <stkutz@gmail.com>
2020-09-18 18:58:05 -0500

    added linked list find loop

---
+++ whiteboard_prob_lect/sockmerchant.py
@@ -0,0 +1,78 @@
+"""
+John works at a clothing store. He has a large pile of socks that he must pair by color for sale. 
+Given an array of integers representing the color of each sock, determine how many pairs of socks with matching colors there are.
+
+For example, there are n = 7 socks with colors ar = [1, 2, 1, 2, 1, 3, 2]. There is one pair of color 1 and one pair of color 2. 
+There are three odd socks left, one of each color. The number of pairs is 2.
+
+Function Description
+
+Complete the sockMerchant function in the editor below. It must return an integer representing the number of matching pairs of socks that are available.
+
+sockMerchant has the following parameter(s):
+
+    n: the number of socks in the pile
+    ar: the colors of each sock
+
+Input Format
+
+The first line contains an integer
+, the number of socks represented in .
+The second line contains space-separated integers describing the colors
+
+of the socks in the pile.
+
+Constraints
+1 <= n <= 100
+a <= ar[i] <= 100   where    0 <= i <= n
+
+Output Format
+
+Return the total number of matching pairs of socks that John can sell.
+
+Sample Input
+
+9
+10 20 20 10 10 30 50 10 20
+
+Sample Output
+
+3
+
+
+"""
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the sockMerchant function below.
+def sockMerchant(n, ar):
+    sock_count = set()
+    num_pairs = 0
+
+    for sock_color in ar:
+        if sock_color in sock_count:
+            num_pairs += 1
+            sock_count.remove(sock_color)
+
+        else:
+            sock_count.add(sock_color)    
+
+    return num_pairs        
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input())
+
+    ar = list(map(int, input().rstrip().split()))
+
+    result = sockMerchant(n, ar)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
