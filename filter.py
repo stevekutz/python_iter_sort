42d789d93a15d7f4f21027ef5326344a785318d4
stevekutz <stkutz@gmail.com>
2020-10-13 19:20:07 -0500

    updated filter.py

--- filter.py
+++ filter.py
@@ -12,7 +12,7 @@ print(f" inside is a list of random chars  >> {rand_chars_list}")
 print(f' letters is list items joined into a string  >>>>>  {letters}')
 
 #########################################################
-######     filter  >>>     filter(function, iterable)    returns in iterator object
+######     filter  >>>     filter(function, iterable)    returns in iterator object of values that test True via a testing function passed in as first argument
 # An iteratable is a Python object that can be used as a sequence. You can go to the next item of the sequence using the next() method.
 # Iterable object types includes lists, strings, dictionaries and sets.
 
@@ -42,69 +42,75 @@ def is_odd(val):
 num_val = '2'
 print(f' {is_odd(num_val)}')
 
-### returns iterator obj
+### filter functions returns iterator obj, assigned to sol here
 sol = filter(is_odd, num_list)
 print(f' sol is {sol}')   # sol is <filter object at 0x7fd8ed7698b0>
 
-#### returns tuple
+#### convert items is iterator into a tuple
 sol_tup = tuple(sol)
 print(f' sol_tup   {sol_tup}')  # (2, 4, 6, 8)
 
-sol_set = set().update('123')
-print(f' sol_set returns    {sol_set} ')
+sol_set = set()
+sol_set.update('123')
+print(f' sol_set returns    {sol_set} ')    # sol_set returns    {'2', '1', '3'}   
 
 
 
 #############
 #####   create very long string of random characters
-letters = ''.join([random.SystemRandom().choice('abcdefghijklmnopqrstuvwxyz') for i in range(10)])
+letters = ''.join([random.SystemRandom().choice('abcdefghijklmnopqrstuvwxyz') for i in range(10000)])
 
 # for i, item in enumerate(letters):
 #     print(f' {i} {item} ')
 
 ####################################################################
-#####    filter function  typically faster   e.g. filter   took 0.0364832878112793
-def vowels(char):
-    vowels = set('aeiou')
+#####    filter method  typically faster  than string remove function  e.g. filter function  took 0.0364832878112793 for 1000000
 
-    if char in vowels:
+def contains_vowels(char):
+    contains_vowels = set('aeiou')
+
+    if char in contains_vowels:
         # print(f' True ')
         return True
     
     return False    
 
 t1 = time.time()
-filtered_vals = list(filter(vowels, letters))
-print(f' filter   took {(time.time() - t1)}')
+filtered_vals = list(filter(contains_vowels, letters))
+print(f' filter method  took {(time.time() - t1)}')
 
 
 ####################################################################
-#####    remove is uusally slow for large lists   e.g. filter   took 0.0364832878112793
+#####    remove method is unusally slow for very large lists   e.g. filter function  ~ took 0.0364832878112793, this takes many minutes
+#####    for 10000 chars, filter method  took 0.0036580562591552734 
+#####                     remove method took  0.10220789909362793    
+
+
 letters_2 = list(letters)
 
 t1 = time.time()
 for ch in letters_2:
-    vowels = set('aeiou')
+    contains_vowels = set('aeiou')
     
-    if ch in vowels:
+    if ch in contains_vowels:
 	    letters_2.remove(ch)
 
-print(f' remove  took {(time.time() - t1)}')
+print(f' remove method took  {(time.time() - t1)}')
 
 # print(f' filtered_vals   {filtered_vals} ')
 
 
-# for i, item in enumerate(filtered_vals):
-#     print(f' {i} item {item}')
+# # for i, item in enumerate(filtered_vals):
+# #     print(f' {i} item {item}')
 
 
-d_list = defaultdict(int)
+# d_list = defaultdict(int)
 
-for k in filtered_vals:
-    d_list[k] += 1
+# for k in filtered_vals:
+#     d_list[k] += 1
 
-print(f' {sorted(d_list.items())} ')
-#  [('a', 379), ('e', 380), ('i', 381), ('o', 381), ('u', 330)] 
+# print(f' {sorted(d_list.items())} ')
+# #  [('a', 379), ('e', 380), ('i', 381), ('o', 381), ('u', 330)] 
 
 
 
